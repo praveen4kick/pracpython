7bf409b5e1c61263001b74ea650a8390e987b538
praveen4kick <praveen4.bachala@gmail.com>
2018-12-16 00:18:38 +0530

    fuck

--- Data Types/Variables/Variables ans functions.py
+++ Data Types/Variables/Variables ans functions.py
@@ -6,9 +6,22 @@ var4="pkk"
 var5="""pkkk"""
 
 
+ab,bc,cd,de,ef,gh=10,10.25,3,"pkk",'pk',"""pkkk"""
+
+
 print(var,type(var),id(var))
 print(var1,type(var1),id(var1))
 print(var2,type(var2),id(var2))
 print(var3,type(var3),id(var3))
 print(var4,type(var4),id(var4))
 print(var5,type(var5),id(var5))
+
+
+
+
+print(ab,type(ab),id(ab))
+print(bc,type(bc),id(bc))
+print(cd,type(cd),id(cd))
+print(de,type(de),id(de))
+print(ef,type(ef),id(ef))
+print(gh,type(gh),id(gh))
