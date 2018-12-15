891cdd3558fa68aff0bf43c31133ab6aaee20898
praveen4kick <praveen4.bachala@gmail.com>
2018-12-15 23:41:47 +0530

    variables

---
+++ Data Types/Variables/Variables ans functions.py
@@ -0,0 +1,14 @@
+var=10
+var1=10.25
+var2='''pkkkk'''
+var3='pk'
+var4="pkk"
+var5="""pkkk"""
+
+
+print(var,type(var),id(var))
+print(var1,type(var1),id(var1))
+print(var2,type(var2),id(var2))
+print(var3,type(var3),id(var3))
+print(var4,type(var4),id(var4))
+print(var5,type(var5),id(var5))
